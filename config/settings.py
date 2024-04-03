16e8a2d2f78d34228ab5b11e4c08c14984d3ce06
omid-salarpour69 <omid.salarpour69@gmail.com>
2024-04-03 19:03:41 +0330

    add login and signup

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # accounts configs
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+CRISPY_TEMPLATE_PACK = 'uni_form'
