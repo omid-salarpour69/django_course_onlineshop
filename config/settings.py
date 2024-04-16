507bccfcac3c73f8e5e99c632968e2a3c8a0fd00
omid-salarpour69 <omid.salarpour69@gmail.com>
2024-04-16 20:05:39 +0330

    add tranlation to site

--- config/settings.py
+++ config/settings.py
@@ -141,12 +141,14 @@ EMAIL_HOST_USER = 'omid.salarpour69@gmail.com'
 EMAIL_HOST_PASSWORD = 'hzch korg ecsx sphd'
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class Product(models.Model):
@@ -26,11 +27,11 @@ class ActiveCommentsManager(models.Manager):
 
 class Comment(models.Model):
     PRODUCT_STARS = [
-        ('1', 'Very Bad'),
-        ('2', 'Bad'),
-        ('3', 'Normal'),
-        ('4', 'Good'),
-        ('5', 'Perfect'),
+        ('1', _('Very Bad')),
+        ('2', _('Bad')),
+        ('3', _('Normal')),
+        ('4', _('Good')),
+        ('5', _('Perfect')),
     ]
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments',)
@@ -40,8 +41,8 @@ class Comment(models.Model):
         related_name='comments', 
         verbose_name='Comment author'
     )
-    body = models.TextField(verbose_name='Comment Text')
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name='Score')
+    body = models.TextField(verbose_name=_('Comment Text'))
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('Score'))
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
