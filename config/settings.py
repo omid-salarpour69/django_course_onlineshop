21ecf2cf68bfecc30b48eb5e2eeed6fb5c0f57de
omid-salarpour69 <omid.salarpour69@gmail.com>
2024-04-23 21:38:39 +0330

    add cart app

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'products.apps.ProductsConfig',
+    'cart.apps.CartConfig',
     # third app
     'crispy_forms',
     "crispy_bootstrap5",
