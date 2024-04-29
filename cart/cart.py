1f2a3e2a4fa3b3a323a6ac1609d35f2eea9f4864
omid-salarpour69 <omid.salarpour69@gmail.com>
2024-04-29 19:35:51 +0330

    inplement cart

---
+++ cart/cart.py
@@ -0,0 +1,75 @@
+from products.models import Product
+
+
+class Cart:
+    def __init__(self, request):
+        """
+        initialize the Cart
+        """
+        self.request = request.user
+
+        self.session = request.session
+
+        cart = self.session.get('cart')
+
+        if not cart:
+            cart = self.session['cart'] = {}
+            # cart = self.session['cart']
+
+        self.cart = cart
+
+    def add(self, product, quantity=1):
+        """
+        Add the specified product to the cart if it exists
+        """
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                'quantity': quantity
+            }
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        self.save()
+
+    def remove(self, product):
+        """
+        Remove the specified product from the cart
+        """
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def save(self):
+        """
+        Mark the cart as modified to save changes
+        """
+        self.session.modified = True
+
+    def __iter__(self):
+        product_id = self.cart.keys()
+        products = Product.objects.filter(id__in=product_id)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product_obj'] = product
+
+        for item in cart.values():
+            yield item
+
+    def __len__(self):
+        return len(self.cart.keys())
+
+    def clear(self):
+        del self.session['cart']
+        self.save()
+
+    def get_total_price(self):
+        product_id = self.cart.keys()
+        products = Product.objects.filter(id__in=product_id)
+
+        return sum(product.price for product in products)
